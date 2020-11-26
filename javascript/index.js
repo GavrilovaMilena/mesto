96924f4f419cdd0b5b2dd29344acf37029e69e5b
Gavrilova Milena <mia.gggg@mail.ru>
2020-11-26 15:03:37 +0300

    Исправлены ошибки после ревью

--- javascript/index.js
+++ javascript/index.js
@@ -1,13 +1,16 @@
 let infoButtonNode = document.querySelector('.info__button');
 let popupNode = document.querySelector('.popup');
 let popupCloseButtonNode = document.querySelector('.popup__close-button');
+let popupFormNode = document.querySelector('.popup__form');
 
+function closePopup() {
+  popupNode.classList.remove('popup_visible');
+}
 
-infoButtonNode.addEventListener('click', handleHeaderButtonClick);
-popupCloseButtonNode.addEventListener('click', handleHeaderButtonClick);
-
-function handleHeaderButtonClick() {
-  popupNode.classList.toggle('popup_visible');
+function openPopup() {
+  popupNode.classList.add('popup_visible');
+  titleInput.value =  infoTitle.textContent;
+  subtitleInput.value = infoSubtitle.textContent;
 }
 
 let titleInput = document.querySelector('.popup__input[name="infoTitle"]');
@@ -15,29 +18,18 @@ let subtitleInput = document.querySelector('.popup__input[name="infoSubtitle"]')
 let infoTitle = document.querySelector('.info__title');
 let infoSubtitle = document.querySelector('.info__subtitle');
 
-
 let popupForm = document.querySelector('.popup__form');
 
-
-function openPopup() {
-  popup.classList.add('popup_opened');
-  titleInput.value = infoTitle.textContent;
-  subtitleInput.value = infoSubtitle.textContent;
-}
-
-function closePopup() {
-  popup.classList.remove('popup_opened');
-}
-
-function formSubmitHandler(evt) {
+function submitHandler(evt) {
   evt.preventDefault();
   infoTitle.textContent = titleInput.value;
   infoSubtitle.textContent = subtitleInput.value;
-  closePopup()
+  closePopup();
 }
 
-popupForm.addEventListener('submit', formSubmitHandler);
-
+popupCloseButtonNode.addEventListener('click', closePopup);
+infoButtonNode.addEventListener('click', openPopup);
+popupFormNode.addEventListener('submit', submitHandler);
 
 
 
