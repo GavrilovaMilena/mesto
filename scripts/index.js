25f661accfd8a9066dd82a1b414d8dda6a90d44b
Gavrilova Milena <mia.gggg@mail.ru>
2020-12-20 19:34:52 +0300

    Проект(4. 5. 6): Место

--- scripts/index.js
+++ scripts/index.js
@@ -89,24 +89,24 @@ popupCardFormNode.addEventListener('submit', submitCardHandler);
 
 const initialCards = [
   {
-    name: 'Архыз',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+    name: 'Москва',
+    link: 'https://images.unsplash.com/photo-1559814925-1cbee7842a7c?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1267&q=80'
   },
   {
-    name: 'Челябинская область',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+    name: 'Роза Хутор',
+    link: 'https://images.unsplash.com/photo-1547925972-ad174a37ac60?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1490&q=80'
   },
   {
-    name: 'Иваново',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+    name: 'Ласточкино гнездо',
+    link: 'https://i.artfile.ru/1920x1170_1090489_[www.ArtFile.ru].jpg'
   },
   {
     name: 'Камчатка',
     link: 'https://images.unsplash.com/photo-1543514060-d333dec37b9f?ixid=MXwxMjA3fDB8MHxzZWFyY2h8Nnx8a2FtY2hhdGthfGVufDB8fDB8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60'
   },
   {
-    name: 'Холмогорский район',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+    name: 'Санкт-Петербург',
+    link: 'https://images.unsplash.com/photo-1586961215517-d2dce511feba?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1268&q=80'
   },
   {
     name: 'Байкал',
